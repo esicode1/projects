8a8fa7695d12c581a940621ebbd7aa7b6c9000f7
esigoo <ehsangb180@gmail.com>
2022-01-10 01:03:31 +0330

    Convert BTC to Dollar with API

---
+++ BitcoinToDollar_notification/app.py
@@ -0,0 +1,26 @@
+import requests
+from notifypy import Notify
+
+
+def get_price_data():
+	'''
+		This function requests to an public API without api key
+		and get bitcoin buy price data.
+		if you get Connection refused error,
+		connect to a safe VPN then run the script :)
+	'''
+	response = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
+	price = float(response.json()['data']['amount'])
+	return price
+
+def sendmessage():
+	notification = Notify()
+	notification.title = "Important"
+	message = 'At this moment bitcoin is ' + str(get_price_data()) + ' dollars'
+	notification.message = message
+	notification.audio = "./bell.wav"
+	notification.send()
+
+
+sendmessage()
+
